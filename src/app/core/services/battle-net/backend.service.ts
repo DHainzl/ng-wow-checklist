f5fbcd9e42e67c1fbe880df4fa88a850087c5ffc
David Hainzl <david@dhainzl.at>
2019-11-17 00:42:41 +0100

    fix: Error handling

--- src/app/core/services/battle-net/backend.service.ts
+++ src/app/core/services/battle-net/backend.service.ts
@@ -1,6 +1,13 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
+import { flatMap } from 'rxjs/operators';
+
+interface ErrorResponse {
+    code: number;
+    type: string;
+    detail: string;
+}
 
 @Injectable({ providedIn: 'root' })
 export class BackendService {
@@ -9,12 +16,23 @@ export class BackendService {
     ) { }
 
     getData<T>(url: string): Observable<T> {
-        return this.http.get<T>(url, {
+        return this.http.get<T | ErrorResponse>(url, {
             headers: new HttpHeaders({
                 Accept: 'application/json',
                 'Content-Type': 'application/json;charset=UTF-8',
             }),
             withCredentials: true,
-        });
+        }).pipe(
+            flatMap(data => {
+                if (this.isErrorResponse(data)) {
+                    return throwError(`Backend returned error: ${data.type} (${data.code} ${data.detail})`);
+                }
+                return of(data);
+            }),
+        );
+    }
+
+    private isErrorResponse<T>(data: T | ErrorResponse): data is ErrorResponse {
+        return data.hasOwnProperty('type') && data.hasOwnProperty('code') && data.hasOwnProperty('detail');
     }
 }
