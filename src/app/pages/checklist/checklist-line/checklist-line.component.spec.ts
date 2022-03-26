95cf01de72194b21d729d4fcc97b3f4e37399c66
David Hainzl <david@dhainzl.at>
2022-03-26 14:55:36 +0100

    ng11 upgrade

--- src/app/pages/checklist/checklist-line/checklist-line.component.spec.ts
+++ src/app/pages/checklist/checklist-line/checklist-line.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ChecklistLineComponent } from './checklist-line.component';
 
@@ -6,7 +6,7 @@ describe('ChecklistLineComponent', () => {
     let component: ChecklistLineComponent;
     let fixture: ComponentFixture<ChecklistLineComponent>;
 
-    beforeEach(async(() => {
+    beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
             declarations: [ ChecklistLineComponent ],
         })
